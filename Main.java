e2df2f2a525fc27fa94a3f7ba331db743d635109
nir nicole <niko426@gmail.com>
2020-01-27 12:29:14 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
 //mmn 14 
 //nir nicole
-//308490945
-
+//main class for phonebook mini project
 public class Main {
-
 	public static void main(String[] args) {
 		new PhoneBookGUI(); //create new phone book instance
-	}
-
-}
+	}//end of main
+}//end of class
