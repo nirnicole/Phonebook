3a768c377ddf683d60b96125eeb81c571a0230aa
nir nicole <niko426@gmail.com>
2020-01-27 12:28:10 +0200

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -1,4 +1,3 @@
-
 //generates contact instances
 public class Contact {
 	
@@ -6,38 +5,24 @@ public class Contact {
 	private String lastName;
 	private String phoneNumber;
 
-		//constructor
+	//constructor
         public Contact(String first, String last, String phoneNumber) {
             this.firstName = first;
             this.lastName = last;
             this.phoneNumber = phoneNumber;
         }
-        
-     
+       
         //getters
         //
-        public String getFirstName() {
-        	return firstName;
-        }
-        
-        public String getLastName() {
-        	return lastName;
-        }
-        
-        public String getPhoneNumber() {
-        	return phoneNumber;
-        }
-        
-        public Object[] getContactObjectArray() {
-        	return new Object[] {firstName,lastName,phoneNumber};
-        }
-
+        public String getFirstName() {return firstName;}
+        public String getLastName() {return lastName;}
+        public String getPhoneNumber() {return phoneNumber;}
+        public Object[] getContactObjectArray() {return new Object[] {firstName,lastName,phoneNumber};}
 
     	//stringing the instance by formal presentation rules separated by tabs.
     	public String toString()
     	{
-    		String str= firstName + "\t" + lastName + "\t" + phoneNumber;				// "name	lastname	phonenumber"
-    			
+    		String str= firstName + "\t" + lastName + "\t" + phoneNumber;		// "name	lastname	phonenumber"
     		return str;
     	}
 
